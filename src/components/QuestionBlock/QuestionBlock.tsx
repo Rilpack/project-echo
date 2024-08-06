8342ccacded9f07f6943746306444f4b1991bb0c
Rilpack <crays01234@gmail.com>
2024-08-06 11:23:38 +0700

    Добавил вывод текста у вопросов. Сделал анимацию появления текста по нажатию на вопрос.

--- src/components/QuestionBlock/QuestionBlock.tsx
+++ src/components/QuestionBlock/QuestionBlock.tsx
@@ -1,18 +1,24 @@
-import styles from './QuestionBlock.module.scss'
+import { useState } from 'react';
+import styles from './QuestionBlock.module.scss';
 
 interface IQuestion {
-  title: string,
-  text: string,
-  icon: string
+  title: string;
+  text: string;
+  icon: string;
 }
 
 export const QuestionBlock = ({ title, text, icon }: IQuestion) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div className={styles.container_question}>
+    <div className={styles.container_question} onClick={() => setIsExpanded(!isExpanded)}>
+      <img style={{ width: 45 }} src={icon} />
       <div className={styles.wrapper_title_question}>
-        <img style={{ width: 45 }} src={icon} />
         <b className={styles.b_title}>{title}</b>
+        <p className={`${styles.p_text} ${isExpanded ? styles.expanded : ''}`}>
+          {text}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
