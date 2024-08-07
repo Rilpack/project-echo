6b8284ae1710e50bb21d9bfb27c8816b381c66a2
Rilpack <crays01234@gmail.com>
2024-08-07 16:45:03 +0700

    Добавил контекст и настроил вывод компонентов на странице личного кабинета.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { BrowserRouter } from 'react-router-dom'
+import { createContext, useState } from 'react';
 import Router from './routes/sections'
 
+interface IUser {
+  name: string,
+  email: string,
+}
+
+interface ModalContextType {
+  user: IUser | null;
+  setUser: React.Dispatch<React.SetStateAction<any>>;
+}
+
+export const ModalContext = createContext<ModalContextType | null>(null);
+
 function App() {
+  const [user, setUser] = useState<IUser | null>(
+    null
+    // { name: "Владимир", email: "email@gmail.com" }
+  );
 
   return (
     <BrowserRouter>
-      <Router />
+      <ModalContext.Provider value={{ user, setUser }}>
+        <Router />
+      </ModalContext.Provider>
     </BrowserRouter>
   )
 }
--- src/pages/Lk/Lk.tsx
+++ src/pages/Lk/Lk.tsx
@@ -1,24 +1,35 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import styles from './Lk.module.scss'
 import { AuthForm } from '../../components/Forms/AuthForm/AuthForm'
 import { RegForm } from '../../components/Forms/RegForm/RegForm'
 import { ButtonTabs } from '../../components/Buttons/ButtonTabs/ButtonTabs'
+import { ModalContext } from '../../App'
 
 export const Lk = () => {
+  // Context
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error('ModalContext must be used within a ModalProvider');
+  }
+  // Context
+
+  const { user, setUser } = context;
   const [form, setForm] = useState<"auth" | "reg">("auth");
 
   return (
     <div className={styles.block_page_container}>
-      <div className={styles.lk_container}>
-        <ButtonTabs form={form} setForm={setForm} />
-        <div className={styles.wrapper_forms}>
-          {
-            form === 'auth'
-              ? <AuthForm />
-              : <RegForm />
-          }
+      {
+        user === null && <div className={styles.lk_container}>
+          <ButtonTabs form={form} setForm={setForm} />
+          <div className={styles.wrapper_forms}>
+            {
+              form === 'auth'
+                ? <AuthForm />
+                : <RegForm />
+            }
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
