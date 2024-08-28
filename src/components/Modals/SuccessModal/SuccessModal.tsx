1d373c35a3adbcad7ed1ffb84dcf6d7fdb9e2cb4
Rilpack <crays01234@gmail.com>
2024-08-28 09:35:27 +0700

    Изменил стилизацию, текст и функционал кнопки в модальном окне об успешной отправке файла.

--- src/components/Modals/SuccessModal/SuccessModal.tsx
+++ src/components/Modals/SuccessModal/SuccessModal.tsx
@@ -1,12 +1,15 @@
 import styles from './SuccessModal.module.scss'
 import close from '../../../assets/images/svg/close.svg'
 import logo from '../../../assets/images/svg/logo.svg'
+import { useNavigate } from 'react-router-dom'
 
 interface ISuccessModal {
   onClose: () => void
 }
 
 export const SuccessModal = ({ onClose }: ISuccessModal) => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.wrapper_success_modal}>
       <div className={styles.container_modal}>
@@ -14,9 +17,9 @@ export const SuccessModal = ({ onClose }: ISuccessModal) => {
           <img onClick={onClose} className={styles.img_close} src={close} />
         </div>
         <div className={styles.wrapper_block}>
-          <h3 className={styles.title_modal}>Мы приступили к расшифровке, проверьте свою почту!</h3>
+          <h3 className={styles.title_modal}>Ваш файл успешно отправлен на расшифровку! Процесс транскрибации начался, и результаты будут доступны в вашем Личном кабинете в ближайшее время</h3>
           <img className={styles.logo_modal} src={logo} />
-          <button onClick={onClose} className={styles.btn_modal}>Закрыть</button>
+          <button onClick={() => { onClose(); navigate("/lk") }} className={styles.btn_modal}>Перейти в "Мои файлы"</button>
         </div>
       </div>
     </div>
